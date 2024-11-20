8429922c0cd1857148ee225ce4ca9fdeab34da0f
Olakareem <olakareemomobolarinwa@gmail.com>
2024-11-20 15:06:06 +0100

    HIM

--- src/app/pbr/test/page.js
+++
@@ -1,6 +0,0 @@
-// pages/page-one.js
-import Dashboard from "@/app/components2/Dashboard";
-
-export default function PageOne() {
-  return <Dashboard dashboardId="a0e5e091-5131-43d2-bfac-81ce6142335e" />;
-}
